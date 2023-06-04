68553b137e2137b87061f427e2d8b712f9687026
IngBryanMartin <ing.bryanmartin@gmail.com>
2023-06-04 15:49:55 -0500

    App.js and App.css v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
 import './App.css';
+import { firestore } from './firebase';
 
 function App() {
+  const [name, setName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (name.length >= 8 && name.length <= 30) {
+      // Guarda el nombre en la base de datos de Firebase
+      firestore.collection('participants').add({ name });
+
+      // Aquí puedes redirigir al participante a la siguiente página del cuestionario
+    } else {
+      setErrorMessage('Es necesario digitar un nombre válido');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>¡ES HORA DE PONER A<br />PRUEBA TUS CONOCIMIENTOS<br />EN CIBERSEGURIDAD!</h1>
+        <h2>¿CREES ESTAR LISTO<br />PARA EL DESAFIO?</h2>
+        <input
+          type="text"
+          placeholder="DIGITA TU NOMBRE COMPLETO"
+          value={name}
+          onChange={handleChange}
+          required
+        />
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <div className="buttons">
+          <button onClick={handleSubmit}>EMPEZAR</button>
+          <a href="https://www.google.com/">VER CLASE</a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0ydDdOexWUYRXQgZrwRWWe6S9yPGXHXA",
+  authDomain: "quiz-cybersecurity.firebaseapp.com",
+  projectId: "quiz-cybersecurity",
+  storageBucket: "quiz-cybersecurity.appspot.com",
+  messagingSenderId: "473257856137",
+  appId: "1:473257856137:web:1e0efe252a5bf1c7314a58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta la instancia de Firestore
+export const firestore = getFirestore(app);
